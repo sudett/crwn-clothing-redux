37025454bedd04c80192f2c1ee3e4fe150fbd0fa
sudett <sudett4978@gmail.com>
2022-01-15 20:54:57 +0330

    made the app fully responsive

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -4,10 +4,10 @@ import "./cart-item.styles.scss";
 
 const CartItem = ({ imageUrl, name, price, quantity }) => (
   <div className="cart-item">
-    <img className="item-img" src={imageUrl} alt="item" />
-    <div className="item-details">
-      <span className="item-name">{name}</span>
-      <span className="item-price">
+    <img className="cart-item__img" src={imageUrl} alt="item" />
+    <div className="cart-item__details">
+      <span className="cart-item__name">{name}</span>
+      <span className="cart-item__price">
         {quantity} X ${price}
       </span>
     </div>
